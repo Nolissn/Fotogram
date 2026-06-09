1f462bd57b862e5de7a21f88f580520d474bea55
Luis Simon <nolissn22222@gmail.com>
2026-06-09 14:23:53 +0200

    refactor: added space images for the project and mad an array that contains the names of the files

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+let pictures = [
+    'black_hole.webp',
+    'blue_giant.webp',
+    'cosmic_net.webp',
+    'galaxy.webp',
+    'nebula.webp',
+    'pulsar.webp',
+    'saturn.webp',
+    'space_exploration.webp',
+    'space_walk.webp',
+    'sun.webp',
+    'triple_star_system.webp',
+    'voyager.webp',
+];
+
+let row_one = document.getElementById('fotos_div_row_one')
+row_one.innerHTML = 'Funktion die HTML Code ausgibt'

7bdf2338b85cd168a2fd705fc62ca3fb08d7c2f0
Luis Simon <nolissn22222@gmail.com>
2026-06-10 14:48:21 +0200

    feat: finished basic layout and added pictures dynamicly with js

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,45 @@
 let pictures = [
-    'black_hole.webp',
-    'blue_giant.webp',
-    'cosmic_net.webp',
-    'galaxy.webp',
-    'nebula.webp',
-    'pulsar.webp',
-    'saturn.webp',
-    'space_exploration.webp',
-    'space_walk.webp',
-    'sun.webp',
-    'triple_star_system.webp',
-    'voyager.webp',
+  "black_hole.webp",
+  "blue_giant.webp",
+  "cosmic_net.webp",
+  "galaxy.webp",
+  "nebula.webp",
+  "pulsar.webp",
+  "saturn.webp",
+  "space_exploration.webp",
+  "space_walk.webp",
+  "sun.webp",
+  "triple_star_system.webp",
+  "voyager.webp",
 ];
 
-let row_one = document.getElementById('fotos_div_row_one')
-row_one.innerHTML = 'Funktion die HTML Code ausgibt'
+function addfirstrow() {
+  let row_one = document.getElementById("pictures_div_row_one");
+  for (let index = 0; index < 7; index++) {
+    row_one.innerHTML += getFirstRowTemplate(index);
+  }
+}
+
+function getFirstRowTemplate(index)
+{
+  return `<div class="thumbnail_div">
+        <img src="./assets/pictures/${pictures[index]}" alt="thumbnail" class="thumbnail_imgage">
+    </div>`;
+}
+
+function addsecondrow() {
+  let row_two = document.getElementById("pictures_div_row_two");
+  for (let index = 7; index < pictures.length; index++) {
+    row_two.innerHTML += getSecondRowTemplate(index);
+  }
+}
+
+function getSecondRowTemplate(index)
+{
+  return `<div class="thumbnail_div">
+        <img src="./assets/pictures/${pictures[index]}" alt="thumbnail" class="thumbnail_imgage">
+    </div>`;
+}
+
+addfirstrow();
+addsecondrow();
